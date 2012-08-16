239df6f28558a41b1a9808d74661bdf652d832c1
John Yang-Sammataro <johnys@stanford.edu>
2012-08-15 23:49:02 -0700

    basic files

--- README.txt
+++ README.txt
@@ -2,3 +2,15 @@ README.txt
 Jim Sproch and John Yang-Sammataro
 "Cookie Jar" An eaiser way to submit, view, and move around code online!
 CS193c Final "Pivot"
+
+Strong UI
+
+BOLD Elements
+Rich colors, XP
+
+Header bar --> non intrusive
+
+
+Space to drop code
+
+A Submit button
---
+++ main.js
@@ -0,0 +1,22 @@
+/* JavaScript for Cookie Jar main.html fileUpload */
+
+/* Correct JLint for the browser and Google libraries */
+/*jslint browser: true, devel: true, plusplus: true, vars: true*/
+/*global getElementById: true*/
+
+/* Globals */
+/* Initialize element variables */
+var fileDropArea = document.getElementById('fileDropArea');
+//Playing with the new CSS3 selectors!
+var canvas = document.querySelector('canvas');
+var context = canvas.getContext('2d'); //Get the 2d API for the canvas element
+var fileCount = document.getElementById('fileCount');
+var uploadUrl = document.getElementById('uploadUrl');
+var uploadStatus = document.getElementById('uploadStatus');
+
+var initHandlers = function () {
+	fileDropArea.addEventListener(
+	fileDropArea.addEventListener(
+	fileDropArea.addEventListener(
+	fileDropArea.addEventListener(
+}
